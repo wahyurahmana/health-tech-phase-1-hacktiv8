3c396a048d4059e6d033d9aef98a4ee71d326ee1
thisRizkiDeniPratama <denipintar172@gmail.com>
2022-03-17 09:20:18 +0700

    fiturPatientDone

--- migrations/20220316090049-create-patient.js
+++ migrations/20220316090049-create-patient.js
@@ -29,7 +29,7 @@ module.exports = {
       DiseaseId: {
         type: Sequelize.INTEGER,
         references: {
-          model: "Patients",
+          model: "Diseases",
           key: "id",
         },
         onUpdate: "cascade",
